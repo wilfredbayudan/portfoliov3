6fae0e1a561289b0aa6cf13165c99b0ef13f3ef0
Wilfred <wilfredbayudan@gmail.com>
2022-04-01 23:48:12 -1000

    Added font

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,22 +6,36 @@ const HeaderWrapper = styled.header``;
 
 const NavBar = styled.nav`
   border-bottom: 1px solid #e5e5e5;
-  padding: 15px;
-  height: 50px;
+  padding: 10px;
+  height: 45px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  @media (min-width: 768px) {
+    padding: 15px;
+    height: 50px;
+  }
 `;
 
 const LogoContainer = styled.div`
   display: flex;
   align-items: center;
   gap: 5px;
+  height: 100%;
   img {
-    height: 50px;
+    height: 100%;
   }
   span {
+    display: none;
     font-family: "Bebas Neue", cursive;
     font-size: 2.5em;
+    @media (min-width: 768px) {
+      display: block;
+    }
   }
 `;
+
+const NavLinks = styled.ul``;
 
 const Header = () => {
   return (
@@ -31,6 +45,7 @@ const Header = () => {
           <img src={Logo} alt="Wilfred Bayudan" />
           <span>AYUDAN</span>
         </LogoContainer>
+        <NavLinks>LINKS</NavLinks>
       </NavBar>
     </HeaderWrapper>
   );
